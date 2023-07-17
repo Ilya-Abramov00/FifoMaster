69ab7badb54be60158bb09b29a81492e416df092
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-07-17 16:02:54 +0300

    в разных main тоже та проблема

--- TestTask/libs/Fifo/test/Fifo_test.cpp
+++ TestTask/libs/Fifo/test/Fifo_test.cpp
@@ -182,3 +182,40 @@ TEST(Fifo, mixing)
 	ASSERT_TRUE(data.size() == n * 12);
 	// вызовы вункции pushwrite перемешаны, пачки данных могли перемешаться
 }
+
+
+TEST(Fifo, time)
+{
+	std::string FIFO2 = "fifo2";
+	int n             = 10;
+	std::string data  = "";
+	data.reserve(n * 12);
+	auto getterRead = [&](void* dataq, size_t szInBytes) {
+		data += std::string((char*)dataq, (char*)dataq + szInBytes);
+	};
+
+	Params params = {
+	    FIFO2,
+	    100,
+	    50,
+	    getterRead,
+	};
+
+	FifoRead client1(params);
+	FifoWrite client2(FIFO2);
+
+	client1.startRead();
+	client2.startWrite();
+
+	for(int i=0;i!=10;i++){
+		std::string da(n,'3');
+		client2.pushData(da.data(),n);
+	}
+
+	sleep(1);
+
+	client2.stopWrite();
+	client1.stopRead();
+	ASSERT_TRUE(data.size() == n*10);
+	ASSERT_TRUE(data == std::string(n*10,'3'));
+}
