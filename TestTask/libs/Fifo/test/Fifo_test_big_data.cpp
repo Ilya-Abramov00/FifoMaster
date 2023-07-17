73903db9853c8828a813dce53c51946f9a6fd612
Ilya-Abramov00 <ilyxa.abramov03@mail.ru>
2023-07-17 16:26:39 +0300

    в разных main тоже та проблема

--- TestTask/libs/Fifo/test/Fifo_test_big_data.cpp
+++ TestTask/libs/Fifo/test/Fifo_test_big_data.cpp
@@ -28,68 +28,51 @@ TEST(big_data, 1)
 	client2.startWrite();
 
 	std::string a0(n, '0');
-	std::thread t([&client2, &a0, &n]() {
-		client2.pushData((void*)a0.data(), n);
-	});
-	t.join();
+
+	client2.pushData((void*)a0.data(), n);
+
 	std::string a(n, '1');
-	std::thread t1([&client2, &a, &n]() {
-		client2.pushData((void*)a.data(), n);
-	});
-	t1.join();
+
+	client2.pushData((void*)a.data(), n);
+
 	std::string b(n, '2');
-	std::thread t2([&client2, &b, &n]() {
-		client2.pushData((void*)b.data(), n);
-	});
-	t2.join();
+
+	client2.pushData((void*)b.data(), n);
+
 	std::string c(n, '3');
-	std::thread t3([&client2, &c, &n]() {
-		client2.pushData((void*)c.data(), n);
-	});
-	t3.join();
+
+	client2.pushData((void*)c.data(), n);
+
 	std::string d(n, '4');
-	std::thread t4([&client2, &d, &n]() {
-		client2.pushData((void*)d.data(), n);
-	});
-	t4.join();
+
+	client2.pushData((void*)d.data(), n);
+
 	std::string e(n, '5');
-	std::thread t5([&client2, &e, &n]() {
-		client2.pushData((void*)e.data(), n);
-	});
-	t5.join();
+
+	client2.pushData((void*)e.data(), n);
+
 	std::string f(n, '6');
-	std::thread t6([&client2, &f, &n]() {
-		client2.pushData((void*)f.data(), n);
-	});
-	t6.join();
+
+	client2.pushData((void*)f.data(), n);
+
 	std::string t7(n, '7');
-	std::thread t71([&client2, &t7, &n]() {
-		client2.pushData((void*)t7.data(), n);
-	});
-	t71.join();
+
+	client2.pushData((void*)t7.data(), n);
+
 	std::string t8(n, '8');
-	std::thread t81([&client2, &t8, &n]() {
-		client2.pushData((void*)t8.data(), n);
-	});
-	t81.join();
+
+	client2.pushData((void*)t8.data(), n);
 
 	std::string t9(n, '9');
-	std::thread t91([&client2, &t9, &n]() {
-		client2.pushData((void*)t9.data(), n);
-	});
-	t91.join();
+
+	client2.pushData((void*)t9.data(), n);
 
 	std::string t10(n, '0');
-	std::thread t101([&client2, &t10, &n]() {
-		client2.pushData((void*)t10.data(), n);
-	});
 
-	t101.join();
+	client2.pushData((void*)t10.data(), n);
+
 	std::string t11(n, '1');
-	std::thread t111([&client2, &t11, &n]() {
-		client2.pushData((void*)t11.data(), n);
-	});
-	t111.join();
+	client2.pushData((void*)t11.data(), n);
 
 	sleep(3);
 
@@ -136,68 +119,51 @@ TEST(big_data, 2)
 	client2.startWrite();
 
 	std::string a0(n, '0');
-	std::thread t([&client2, &a0, &n]() {
-		client2.pushData((void*)a0.data(), n);
-	});
-	t.join();
+
+	client2.pushData((void*)a0.data(), n);
+
 	std::string a(n, '1');
-	std::thread t1([&client2, &a, &n]() {
-		client2.pushData((void*)a.data(), n);
-	});
-	t1.join();
+
+	client2.pushData((void*)a.data(), n);
+
 	std::string b(n, '2');
-	std::thread t2([&client2, &b, &n]() {
-		client2.pushData((void*)b.data(), n);
-	});
-	t2.join();
+
+	client2.pushData((void*)b.data(), n);
+
 	std::string c(n, '3');
-	std::thread t3([&client2, &c, &n]() {
-		client2.pushData((void*)c.data(), n);
-	});
-	t3.join();
+
+	client2.pushData((void*)c.data(), n);
+
 	std::string d(n, '4');
-	std::thread t4([&client2, &d, &n]() {
-		client2.pushData((void*)d.data(), n);
-	});
-	t4.join();
+
+	client2.pushData((void*)d.data(), n);
+
 	std::string e(n, '5');
-	std::thread t5([&client2, &e, &n]() {
-		client2.pushData((void*)e.data(), n);
-	});
-	t5.join();
+
+	client2.pushData((void*)e.data(), n);
+
 	std::string f(n, '6');
-	std::thread t6([&client2, &f, &n]() {
-		client2.pushData((void*)f.data(), n);
-	});
-	t6.join();
+
+	client2.pushData((void*)f.data(), n);
+
 	std::string t7(n, '7');
-	std::thread t71([&client2, &t7, &n]() {
-		client2.pushData((void*)t7.data(), n);
-	});
-	t71.join();
+
+	client2.pushData((void*)t7.data(), n);
+
 	std::string t8(n, '8');
-	std::thread t81([&client2, &t8, &n]() {
-		client2.pushData((void*)t8.data(), n);
-	});
-	t81.join();
+
+	client2.pushData((void*)t8.data(), n);
 
 	std::string t9(n, '9');
-	std::thread t91([&client2, &t9, &n]() {
-		client2.pushData((void*)t9.data(), n);
-	});
-	t91.join();
+
+	client2.pushData((void*)t9.data(), n);
 
 	std::string t10(n, '0');
-	std::thread t101([&client2, &t10, &n]() {
-		client2.pushData((void*)t10.data(), n);
-	});
 
-	t101.join();
+	client2.pushData((void*)t10.data(), n);
+
 	std::string t11(n, '1');
-	std::thread t111([&client2, &t11, &n]() {
-		client2.pushData((void*)t11.data(), n);
-	});
-	t111.join();
+	client2.pushData((void*)t11.data(), n);
 
 	sleep(3);
 
