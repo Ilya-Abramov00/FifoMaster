52c013d1db5c204d2f89c782688fa2ad8b4445a1
Ilya-Abramov00 <@Ilya-Abramov00>
2023-07-20 17:24:35 +0300

    s

--- Server.cpp
+++ Server.cpp
@@ -5,30 +5,36 @@
 
 int main()
 {
-	std::cout << "сервер" << std::endl;
+	std::cout << "клиент" << std::endl << std::endl;
 
 	std::string FIFO2 = "/home/ilya/fifo2";
+	std::string FIFO1 = "/home/ilya/fifo1";
+	int n             = 100;
+	std::string data  = "";
+	data.reserve(n * 1024);
 
-	int n            = 10;
-	std::string data = "";
-	data.reserve(n * 10);
 	auto getterRead = [&](Data&& dataq) {
-		auto a = std::string(dataq.data(), dataq.data() + dataq.size());
-		data += a;
-		std::cout << a << std::endl;
-		std::cout << dataq.size() << std::endl;
+		data += std::string(dataq.data(), dataq.data() + dataq.size());
 	};
 	auto connect = []() {
 		std::cout << "произошел коннект" << std::endl;
 	};
 
-	Fifo client1(FIFO2, FIFO2);
+	Fifo client1(FIFO2, FIFO1);
 	client1.setReadHandler(getterRead);
 	client1.setConnectionHandler(connect);
+	client1.start();
 
-	//	std::this_thread::sleep_for(std::chrono::seconds(5));
+	std::string a(n, 'a');
 
-	std::cout << "\nсервер окончил прием\n" << std::endl;
-	//	std::cout << data;
+	for(int i = 0; i != 10; i++) {
+		client1.write((void*)a.data(), n);
+	}
+	sleep(5);
+	client1.stop();
+
+	std::cout << "клиент завершил отправку" << std::endl;
+
+	std::cout << data.size();
 	return 1;
 }
--- TestTask/libs/Fifo/test/Fifo_test_big_data_FIFO.cpp
+++ TestTask/libs/Fifo/test/Fifo_test_big_data_FIFO.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-TEST(big_data, 1)
+TEST(big_data_FIFO, 1)
 {
 	std::string FIFO2 = "fifo2";
 	int n             = 1024 * 1024 * 120;
@@ -16,66 +16,62 @@ TEST(big_data, 1)
 	auto connect = []() {
 
 	};
-	FifoRead client1(FIFO2);
-	client1.setReadHandler(getterRead);
-	client1.setConnectionHandler(connect);
+	Fifo client2(FIFO2, FIFO2);
+	client2.setReadHandler(getterRead);
+	client2.setConnectionHandler(connect);
 
-	FifoWrite client2(FIFO2);
-
-	client1.startRead();
-	client2.startWrite();
+	client2.start();
 
 	std::string a0(n, '0');
 
-	client2.pushData((void*)a0.data(), n);
+	client2.write((void*)a0.data(), n);
 
 	std::string a(n, '1');
 
-	client2.pushData((void*)a.data(), n);
+	client2.write((void*)a.data(), n);
 
 	std::string b(n, '2');
 
-	client2.pushData((void*)b.data(), n);
+	client2.write((void*)b.data(), n);
 
 	std::string c(n, '3');
 
-	client2.pushData((void*)c.data(), n);
+	client2.write((void*)c.data(), n);
 
 	std::string d(n, '4');
 
-	client2.pushData((void*)d.data(), n);
+	client2.write((void*)d.data(), n);
 
 	std::string e(n, '5');
 
-	client2.pushData((void*)e.data(), n);
+	client2.write((void*)e.data(), n);
 
 	std::string f(n, '6');
 
-	client2.pushData((void*)f.data(), n);
+	client2.write((void*)f.data(), n);
 
 	std::string t7(n, '7');
 
-	client2.pushData((void*)t7.data(), n);
+	client2.write((void*)t7.data(), n);
 
 	std::string t8(n, '8');
 
-	client2.pushData((void*)t8.data(), n);
+	client2.write((void*)t8.data(), n);
 
 	std::string t9(n, '9');
 
-	client2.pushData((void*)t9.data(), n);
+	client2.write((void*)t9.data(), n);
 
 	std::string t10(n, '0');
 
-	client2.pushData((void*)t10.data(), n);
+	client2.write((void*)t10.data(), n);
 
 	std::string t11(n, '1');
-	client2.pushData((void*)t11.data(), n);
+	client2.write((void*)t11.data(), n);
 
 	sleep(3);
 
-	client2.stopWrite();
-	client1.stopRead();
+	client2.stop();
 
 	a = a0 + a;
 	a += b;
@@ -92,10 +88,10 @@ TEST(big_data, 1)
 	ASSERT_TRUE(data.size() == n * 12);
 	ASSERT_TRUE(data == a);
 }
-TEST(big_data, 2)
+TEST(big_data_FIFO, 2)
 {
 	std::string FIFO2 = "fifo2";
-	int n             = 1024 * 1024;
+	int n             = 1024*1024;
 	std::string data  = "";
 	data.reserve(n * 12);
 	auto getterRead = [&](Data&& dataq) {
@@ -104,66 +100,62 @@ TEST(big_data, 2)
 	auto connect = []() {
 
 	};
-	FifoRead client1(FIFO2);
-	client1.setReadHandler(getterRead);
-	client1.setConnectionHandler(connect);
+	Fifo client2(FIFO2, FIFO2);
+	client2.setReadHandler(getterRead);
+	client2.setConnectionHandler(connect);
 
-	FifoWrite client2(FIFO2);
-
-	client1.startRead();
-	client2.startWrite();
+	client2.start();
 
 	std::string a0(n, '0');
 
-	client2.pushData((void*)a0.data(), n);
+	client2.write((void*)a0.data(), n);
 
 	std::string a(n, '1');
 
-	client2.pushData((void*)a.data(), n);
+	client2.write((void*)a.data(), n);
 
 	std::string b(n, '2');
 
-	client2.pushData((void*)b.data(), n);
+	client2.write((void*)b.data(), n);
 
 	std::string c(n, '3');
 
-	client2.pushData((void*)c.data(), n);
+	client2.write((void*)c.data(), n);
 
 	std::string d(n, '4');
 
-	client2.pushData((void*)d.data(), n);
+	client2.write((void*)d.data(), n);
 
 	std::string e(n, '5');
 
-	client2.pushData((void*)e.data(), n);
+	client2.write((void*)e.data(), n);
 
 	std::string f(n, '6');
 
-	client2.pushData((void*)f.data(), n);
+	client2.write((void*)f.data(), n);
 
 	std::string t7(n, '7');
 
-	client2.pushData((void*)t7.data(), n);
+	client2.write((void*)t7.data(), n);
 
 	std::string t8(n, '8');
 
-	client2.pushData((void*)t8.data(), n);
+	client2.write((void*)t8.data(), n);
 
 	std::string t9(n, '9');
 
-	client2.pushData((void*)t9.data(), n);
+	client2.write((void*)t9.data(), n);
 
 	std::string t10(n, '0');
 
-	client2.pushData((void*)t10.data(), n);
+	client2.write((void*)t10.data(), n);
 
 	std::string t11(n, '1');
-	client2.pushData((void*)t11.data(), n);
+	client2.write((void*)t11.data(), n);
 
 	sleep(3);
 
-	client2.stopWrite();
-	client1.stopRead();
+	client2.stop();
 
 	a = a0 + a;
 	a += b;
