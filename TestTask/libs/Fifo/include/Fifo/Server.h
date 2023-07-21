b326fdcc26bcb7a1612a9da09fcb422dd35c1542
Ilya-Abramov00 <@Ilya-Abramov00>
2023-07-21 23:29:34 +0300

    s

--- TestTask/libs/Fifo/include/Fifo/Server.h
+++ TestTask/libs/Fifo/include/Fifo/Server.h
@@ -21,46 +21,39 @@
 
 #include "Fifo/FifoBase.h"
 
-class Fifo {
+class Server {
 public:
-	Fifo(const std::string fdFileNameWrite, const std::string fdFileNameRead);
+	using ConnectionId          = std::unordered_map<std::string, std::shared_ptr<Fifo>>;
+	using ConnChangeHandler     = std::function<void(ConnectionId)>;
+	using ReadHandler           = std::function<void(ConnectionId, FifoRead::Data&&)>;
+	using IdDistributionHandler = std::function<ConnectionId()>;
 
-	void setConnectionHandlerRead(FifoBase::ConnectionHandler handler);
-	void setDisConnectionHandlerRead(FifoBase::ConnectionHandler handler);
+	Server(const std::vector<std::string>& nameChannelsFifo);
 
-	void setConnectionHandlerWrite(FifoBase::ConnectionHandler handler);
-	void setDisConnectionHandlerWrite(FifoBase::ConnectionHandler handler);
+	void setReadHandler(ReadHandler h);
 
-	void setReadHandler(FifoRead::ReadHandler handler);
-	bool const getWaitDisConnectRead() const
-	{
-		return fifoRead.getWaitDisConnect();
-	}
-	bool const getWaitConnectRead() const
-	{
-		return fifoRead.getWaitConnect();
-	}
-	bool const getWaitDisConnectWrite() const
-	{
-		return fifoWrite.getWaitDisConnect();
-		;
-	}
-	bool const getWaitConnectWrite() const
-	{
-		return fifoWrite.getWaitConnect();
-	}
+	void setNewConnectionHandler(ConnChangeHandler h);
+	void setCloseConnectionHandler(ConnChangeHandler h);
 
-	void write(const void* data, size_t sizeInBytes);
+	void write(std::shared_ptr<Fifo> object, const void* data, size_t sizeInBytes);
 
 	void start();
 	void stop();
+	ConnectionId connectionId;
 
 private:
-	FifoWrite fifoWrite;
-	FifoRead fifoRead;
+	void getter(FifoRead::Data&& data);
+
+	void logicConnect(std::shared_ptr<Fifo> object);
+	void logicDisConnect(std::shared_ptr<Fifo> object);
+
+	const std::vector<std::string>& nameChannelsFifo;
+
+	ReadHandler readHandler;
+
+	ConnChangeHandler newHandler;
+	ConnChangeHandler closeHandler;
 };
-
-
 
 // virtual ~Server() = default;
 // virtual void start()                                             = 0;
