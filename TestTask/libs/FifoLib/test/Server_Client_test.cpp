c5733d69fb629a0c1fee06b9d39f044884c586e6
Ilya-Abramov00 <@Ilya-Abramov00>
2023-07-25 23:18:33 +0300

    QMAIN-260 рефакторинг FifoServer, FifoClient

--- TestTask/libs/FifoLib/test/Server_Client_test.cpp
+++ TestTask/libs/FifoLib/test/Server_Client_test.cpp
@@ -10,153 +10,131 @@ public:
 	void SetUp() override
 	{}
 
-	void startWriteClient(Client& client, int n)
+	void clients(FifoCfg e, int n, int k)
 	{
-		client.start();
+		Client client(e);
+		std::string dataClient1 = "";
 
-		std::string data0(n, 'v');
-		for(int i = 0; i != 10; i++) {
-			client.write((void*)data0.data(), n);
-			sleep(1);
-		}
-		sleep(20);
-		client.stop();
+		auto getterClient1 = [&dataClient1](FifoRead::Data&& dataq) {
+			dataClient1.insert(dataClient1.begin(), dataq.data(), dataq.data() + dataq.size());
+		};
+		bool clientConnection{false};
+		bool clientDisconnection{false};
+		client.setReadHandler(getterClient1);
+		client.setConnectHandler([&clientConnection]() {
+			clientConnection = true;
+		});
+		client.setDisconnectHandler([&clientDisconnection]() {
+			clientDisconnection = true;
+		});
+
+		startWriteClient(client, n, k);
+
+		ASSERT_TRUE(clientConnection == true);
+		ASSERT_TRUE(clientDisconnection == true);
+		ASSERT_TRUE(dataClient1.size() == n * k);
 	}
-
-	void startWriteServer(Server& server, int n)
+	void servers(std::list<FifoCfg> s, int n, int k, size_t client, std::mutex& mtx0)
 	{
-		server.start();
-		std::string data0(n, 'a');
-		for(int i = 0; i != 10; i++) {
-			server.write(0, (void*)data0.data(), n);
-			server.write(1, (void*)data0.data(), n);
-			server.write(2, (void*)data0.data(), n);
-			sleep(1);
-		}
-		sleep(30);
+		std::string dataServer = "";
+		auto getterServer      = [&dataServer, &mtx0](size_t id, FifoRead::Data&& dataq) {
+            std::lock_guard<std::mutex> mtx(mtx0);
+            dataServer.insert(dataServer.end(), dataq.data(), dataq.data() + dataq.size());
+		};
 
-		server.stop();
+		Server server(s);
+		int serverConnection    = 0;
+		int serverDisconnection = 0;
+		server.setReadHandler(getterServer);
+		server.setConnectHandler([&serverConnection](size_t) {
+			serverConnection += 1;
+		});
+		server.setDisconnectHandler([&serverDisconnection](size_t) {
+			serverDisconnection += 1;
+		});
+
+		startWriteServer(server, n, k, client);
+		ASSERT_TRUE(serverConnection == client);
+		ASSERT_TRUE(serverDisconnection == client);
+		ASSERT_TRUE(dataServer.size() == client * n * k);
 	}
 
 	void TearDown() override
 	{}
 
 private:
-};
+	void startWriteServer(Server& server, int n, int k, size_t client)
+	{
+		server.start();
+		std::string data0(n, 'a');
+		for(int i = 0; i != k; i++) {
+			for(int k = 0; k != client; k++) {
+				server.write(k, (void*)data0.data(), n);
+			}
+		}
+		sleep(20);
 
+		server.stop();
+	}
+	void startWriteClient(Client& client, int n, int k)
+	{
+		client.start();
+
+		std::string data0(n, 'v');
+		for(int i = 0; i != k; i++) {
+			client.write((void*)data0.data(), n);
+		}
+		sleep(15);
+		client.stop();
+	}
+};
+std::string FIFO5 = "fifo5";
+std::string FIFO4 = "fifo4";
 std::string FIFO3 = "fifo3";
 std::string FIFO2 = "fifo2";
 std::string FIFO1 = "fifo1";
 
-FifoCfg k1{FIFO1, FIFO1 + "_reverse"};
-FifoCfg k2{FIFO2, FIFO2 + "_reverse"};
-FifoCfg k3{FIFO3, FIFO3 + "_reverse"};
-
-TEST_F(ServerClientTest, Clients3To1ServerConnectin)
+TEST_F(ServerClientTest, Clients5To1ServerConnectin)
 {
-	int n                  = 1024 * 1024;
-	std::string dataServer = "";
+	FifoCfg k1{FIFO1, FIFO1 + "_reverse"};
+	FifoCfg k2{FIFO2, FIFO2 + "_reverse"};
+	FifoCfg k3{FIFO3, FIFO3 + "_reverse"};
+	FifoCfg k4{FIFO4, FIFO4 + "_reverse"};
+	FifoCfg k5{FIFO5, FIFO5 + "_reverse"};
 
-	bool serverConnection{false};
-	bool serverDisonnection{false};
+	std::list<FifoCfg> data = {k1, k2, k3, k4, k5};
 
-	std::mutex mtx0;
-	auto getterServer = [&dataServer, &mtx0](size_t id, FifoRead::Data&& dataq) {
-		std::lock_guard<std::mutex> mtx(mtx0);
-		dataServer.insert(dataServer.end(), dataq.data(), dataq.data() + dataq.size());
-	};
+	int sizeN = 1024 * 1024;
+	int n     = 10;
 
-	Server server({k1, k2, k3});
-
-	server.setReadHandler(getterServer);
-	server.setConnectHandler([&serverConnection](size_t) {
-		serverConnection = true;
-	});
-	server.setDisconnectHandler([&serverDisonnection](size_t) {
-		serverDisonnection = true;
+	std::mutex mtx;
+	std::thread tServer([data, &sizeN, n, this, &mtx]() {
+		servers(data, sizeN, n, data.size(), mtx);
 	});
 
-	Client client1(k1);
-	std::string dataClient1 = "";
-
-	auto getterClient1 = [&dataClient1](FifoRead::Data&& dataq) {
-		dataClient1.insert(dataClient1.begin(), dataq.data(), dataq.data() + dataq.size());
-	};
-	bool client1Connection{false};
-	bool client1Disconnection{false};
-	client1.setReadHandler(getterClient1);
-	client1.setConnectHandler([&client1Connection]() {
-		client1Connection = true;
-	});
-	client1.setDisconnectHandler([&client1Disconnection]() {
-        client1Disconnection = true;
+	std::thread tClient1([&k1, &sizeN, n, this]() {
+		clients(k1, sizeN, n);
 	});
 
-	Client client2(k2);
-	std::string dataClient2 = "";
-	auto getterClient2      = [&dataClient2](FifoRead::Data&& dataq) {
-        dataClient2.insert(dataClient2.begin(), dataq.data(), dataq.data() + dataq.size());
-	};
-	bool client2Connection{false};
-	bool client2Disconnection{false};
-	client2.setReadHandler(getterClient2);
-	client2.setConnectHandler([&client2Connection]() {
-		client2Connection = true;
-	});
-	client2.setDisconnectHandler([&client2Disconnection]() {
-        client2Disconnection = true;
+	std::thread tClient2([&k2, &sizeN, n, this]() {
+		clients(k2, sizeN, n);
 	});
 
-	Client client3(k3);
-	std::string dataClient3 = "";
-	auto getterClient3      = [&dataClient3](FifoRead::Data&& dataq) {
-        dataClient3.insert(dataClient3.begin(), dataq.data(), dataq.data() + dataq.size());
-	};
-	bool client3Connection{false};
-	bool client3Disconnection{false};
-	client3.setReadHandler(getterClient3);
-	client3.setConnectHandler([&client3Connection]() {
-		client3Connection = true;
+	std::thread tClient3([&k3, &sizeN, n, this]() {
+		clients(k3, sizeN, n);
 	});
-	client3.setDisconnectHandler([&client3Disconnection]() {
-        client3Disconnection = true;
+	std::thread tClient4([&k4, &sizeN, n, this]() {
+		clients(k4, sizeN, n);
 	});
-
-	std::thread tServer([&server, &n, this]() {
-		startWriteServer(server, n);
-	});
-
-	std::thread tClient1([&client1, &n, this]() {
-		startWriteClient(client1, n);
-	});
-
-	std::thread tClient2([&client2, &n, this]() {
-		startWriteClient(client2, n);
-	});
-
-	std::thread tClient3([&client3, n, this]() {
-		startWriteClient(client3, n);
+	std::thread tClient5([&k5, &sizeN, n, this]() {
+		clients(k5, sizeN, n);
 	});
 
 	tClient1.join();
 	tClient2.join();
 	tClient3.join();
+	tClient4.join();
+	tClient5.join();
+
 	tServer.join();
-
-	ASSERT_TRUE(serverConnection == true);
-	ASSERT_TRUE(serverDisonnection == true);
-
-	ASSERT_TRUE(client1Connection == true);
-	ASSERT_TRUE(client1Disconnection == true);
-
-	ASSERT_TRUE(client2Connection == true);
-	ASSERT_TRUE(client2Disconnection == true);
-
-	ASSERT_TRUE(client3Connection == true);
-	ASSERT_TRUE(client3Disconnection == true);
-
-	ASSERT_TRUE(dataServer.size() == 3 * n * 10);
-	ASSERT_TRUE(dataClient1.size() == n * 10);
-	ASSERT_TRUE(dataClient2.size() == n * 10);
-	ASSERT_TRUE(dataClient3.size() == n * 10);
 }
